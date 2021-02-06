66f4ab0791a5a0281f29b15662808c2801df10a9
Steve Schaner <schanerst@gmail.com>
2021-02-06 07:59:11 -0500

    Finished with 'Section 12 - Admin Screens - Part 2'

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc        Update order to delivered
+// @route       GET /api/orders/:id/deliver
+// @access      Private/Admin
+const updateOrderToDeliered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order && (req.user.isAdmin || order.user._id.equals(req.user._id))) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 // @desc        Get logged in user orders
 // @route       GET /api/orders/myorders
 // @access      Private
@@ -87,4 +106,12 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc        Get all orders
+// @route       GET /api/orders
+// @access      Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, updateOrderToDeliered, getMyOrders, getOrders };

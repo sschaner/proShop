722e6cec0e447b7b48d311c6c012be8ae88b4e25
Steve Schaner <schanerst@gmail.com>
2021-02-22 17:40:19 -0500

    Added meta tags to index.html

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -8,6 +8,7 @@ const Meta = ({
   productTitle,
   author,
   ogImage,
+  ogImageSecure,
 }) => {
   return (
     <Helmet>
@@ -16,23 +17,6 @@ const Meta = ({
       </title>
       <meta name='description' content={description} />
       <meta name='keywords' content={keywords} />
-      <meta name='author' content={author} />
-      <meta property='og:type' content='website' />
-      <meta property='og:title' content={title} />
-      <meta property='og:image:secure_url' content={ogImageSecure} />
-      <meta property='og:image:width' content='1910' />
-      <meta property='og:image:height' content='901' />
-      <meta
-        property='og:image:alt'
-        content='Screenshot of the ProShop home page'
-      />
-      <meta
-        property='og:url'
-        content='https://schanerproshopapp.herokuapp.com/'
-      />
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta property='og:site_name' content='ProShop' />
-      <meta name='twitter:image:alt' content='ProShop' />
     </Helmet>
   );
 };
@@ -42,11 +26,6 @@ Meta.defaultProps = {
   description:
     'We sell the best products for cheap. This is a fake eCommerce site. Nothing here is really for sale. This is a MERN eCommerce project.',
   keywords: 'electronics, buy electronics, cheap electronics',
-  author: 'Steve Schaner',
-  ogImage:
-    'https://schanerproshopapp.herokuapp.com/images/proshop-app-screenshot.png',
-  ogImageSecure:
-    'https://schanerproshopapp.herokuapp.com/images/proshop-app-screenshot.png',
   productTitle: '',
 };
 

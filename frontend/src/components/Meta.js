bcb13341d2b5dbba1cd7db7f377cb69313ffd72a
Steve Schaner <schanerst@gmail.com>
2021-02-22 16:22:25 -0500

    Changed meta tags

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -9,6 +9,34 @@ const Meta = ({ title, description, keywords, productTitle }) => {
       </title>
       <meta name='description' content={description} />
       <meta name='keywords' content={keywords} />
+      <meta name='author' content='Steven Schaner' />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content='ProShop' />
+      <meta
+        property='og:description'
+        content='We sell the best products for cheap.'
+      />
+      <meta
+        property='og:image'
+        content='https://schanerproshopapp.herokuapp.com/images/proshop-app-screenshot.png'
+      />
+      <meta
+        property='og:image:secure_url'
+        content='https://schanerproshopapp.herokuapp.com/images/proshop-app-screenshot.png'
+      />
+      <meta property='og:image:width' content='1910' />
+      <meta property='og:image:height' content='901' />
+      <meta
+        property='og:image:alt'
+        content='Screenshot of the ProShop home page'
+      />
+      <meta
+        property='og:url'
+        content='https://schanerproshopapp.herokuapp.com/'
+      />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='og:site_name' content='ProShop' />
+      <meta name='twitter:image:alt' content='ProShop' />
     </Helmet>
   );
 };
